93bc3c018be95d49f865d7b262a72a8132496e47
FelipeCardos <50990842+FelipeCardos@users.noreply.github.com>
2022-04-26 21:42:10 +0100

    j

--- Futoshiki.py
+++ Futoshiki.py
@@ -7,12 +7,13 @@ def Futoshiki(n):
     variables = [x for x in range(n)]
     domains = {}
     neighbors = ''
-
+    restricoes = {}
     for variable in variables:
         domains[variable] = [(x, y) for x in range(n) for y in range(n)]
         neighbors+= str(variable) + ' : ' + str(domains[variable])
-    neighbors = parse_neeighbors(neighbors)
+    neighbors = parse_neighbors(neighbors)
 
     def restricoes_futoshiki(x, y, linha, coluna):
         if linha:
+            
             
