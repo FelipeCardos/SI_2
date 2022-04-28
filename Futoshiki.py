22a19ef164591454e2f8623d4b8b7a44717269b2
FelipeCardos <50990842+FelipeCardos@users.noreply.github.com>
2022-04-28 11:13:58 +0100

    o

--- Futoshiki.py
+++ Futoshiki.py
@@ -9,16 +9,25 @@ def Futoshiki(n):
     neighbors = ''
     restricoes = {}
     for variable in variables:
-        domains[variable] = [(x, y) for x in range(n) for y in range(n)]
-        neighbors+= str(variable) + ' : ' + str(domains[variable])
-    neighbors = parse_neighbors(neighbors)
+        domains[variable] = [(x, y) for x in range(1,n+1) for y in range(1,n+1)]
+        neighbors+= variable + ":" 
+        for v in variables:
+            if v != variable:
+                neighbors+=" "+v
+        neighbors+=";"
+    neighbors = parse_neighbors(neighbors[:-1])
+    print(neighbors)
+    print("==============================================================")
+    print(domains)
+    print("==============================================================")
+    # def restricoes_futoshiki(x, y, a, b):
+    #     if x in variables and y in linha or \
+    #        x in variables and y in coluna:
+    #        restricoes[(x,y)] = diferentes
 
-    def restricoes_futoshiki(x, y, a, b):
-        if x in variables and y in linha or \
-           x in variables and y in coluna:
-           restricoes[(x,y)] = diferentes
+    #     if (x,y) in restricoes:
+    #         return restricoes[(x,y)(a,b)]
 
-        if (x,y) in restricoes:
-            return restricoes[(x,y)(a,b)]
+    # return CSP(variables, domains, neighbors, restricoes_futoshiki)
 
-    return CSP(variables, domains, neighbors, restricoes_futoshiki)
+Futoshiki(3)
